eb1e17b18bee3d06ca313d072c84670d6a92ad49
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-03 15:05:56 +0530

    Update dynamic_dropdown_1.java

--- project1-automation/src/main/java/with TESTNG/dropdown/dynamic_dropdown_1.java
+++ project1-automation/src/main/java/with TESTNG/dropdown/dynamic_dropdown_1.java
@@ -25,7 +25,7 @@ public class Day3_dynamic_dropdown_1_testng {
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='DEL']")).click(); // in this i used parent child relationship xpath locator.
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[contains(text(),'Goa')]")).click();
+		driver.findElement(By.xpath("(//a[@value='GOI'])[2]")).click(); // in this i used the index for the xpath locator 
 		driver.close();
 		
 		
