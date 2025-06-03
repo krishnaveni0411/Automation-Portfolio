0563ecad36481443173dfa54e75cd66c10293f14
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-03 16:20:02 +0530

    Update dynamic_dropdown_1.java

--- project1-automation/src/main/java/without TESTNG FreamWork/dropdown/dynamic_dropdown_1.java
+++ project1-automation/src/main/java/without TESTNG FreamWork/dropdown/dynamic_dropdown_1.java
@@ -1,1 +1,27 @@
-test
+package automationtestingwithudemy;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day3_dynamic_dropdown_1 {
+	
+	public void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='DEL']")).click(); // in this i used parent child relationship xpath locator.
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[contains(text(),'Goa')]")).click();
+		driver.close();
+		
+		
+	}
+
+}
+
