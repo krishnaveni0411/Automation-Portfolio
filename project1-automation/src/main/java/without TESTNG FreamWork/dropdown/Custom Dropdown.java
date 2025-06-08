5d853fa858ca811358e1088ae440128bf112d8f7
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-08 19:14:55 +0530

    Create Custom Dropdown.java

---
+++ project1-automation/src/main/java/without TESTNG FreamWork/dropdown/Custom Dropdown.java
@@ -0,0 +1,44 @@
+package automationtestingwithudemy;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Day5_Custom_Dropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// in this we are going to handle "Custom Dropdown with Increment/Decrement Controls"
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		WebElement dropdown = driver.findElement(By.id("divpaxinfo"));
+		System.out.println(dropdown.getText());
+		dropdown.click();
+	// here i want to click the increment button for 4 time iam going to do it in two ways 
+		//1st way -> to incrementing it
+		int i=1;
+		while(i<5) {
+			driver.findElement(By.id("hrefIncAdt")).click();
+			i++;
+		}
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@value='Done']")).click();
+		System.out.println(dropdown.getText());
+		Thread.sleep(1000);
+		// here iam again click the dropdown and decreasing the count
+		dropdown.click();
+		//2nd way -> to decrement it
+		for(int j=1; j<5; j++) {
+			driver.findElement(By.id("hrefDecAdt")).click();
+		}
+		driver.findElement(By.xpath("//input[@value='Done']")).click();		
+		System.out.println(dropdown.getText());
+		driver.close();
+	}
+
+}
