5fbf6011ced48ec6b0948ea3d6b948b8da097d3b
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-08 20:31:28 +0530

    Update Custom Dropdown.java

--- project1-automation/src/main/java/with TESTNG/dropdown/Custom Dropdown.java
+++ project1-automation/src/main/java/with TESTNG/dropdown/Custom Dropdown.java
@@ -43,7 +43,9 @@ public class Day5_Custom_Dropdown_Testng {
 		for(int j=1; j<5; j++) {
 			driver.findElement(By.id("hrefDecAdt")).click();
 		}
-		driver.findElement(By.xpath("//input[@value='Done']")).click();		
+		driver.findElement(By.xpath("//input[@value='Done']")).click();	
+		Assert.assertEquals(dropdown.getText(), "5 Adult");
+		//if you want to print the value in console then go with System.out.println();
 		System.out.println(dropdown.getText());
 		driver.close();
 	}
