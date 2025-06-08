17dd7c549a6f9bd843d996a10df4e77739ff572a
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-08 20:34:33 +0530

    Update Custom Dropdown.java

--- project1-automation/src/main/java/without TESTNG FreamWork/dropdown/Custom Dropdown.java
+++ project1-automation/src/main/java/without TESTNG FreamWork/dropdown/Custom Dropdown.java
@@ -28,6 +28,8 @@ public class Day5_Custom_Dropdown {
 		}
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@value='Done']")).click();
+		Assert.assertEquals(dropdown.getText(), "5 Adult");
+		//if you want to print the value in console then go with System.out.println();
 		System.out.println(dropdown.getText());
 		Thread.sleep(1000);
 		// here iam again click the dropdown and decreasing the count
@@ -37,7 +39,7 @@ public class Day5_Custom_Dropdown {
 			driver.findElement(By.id("hrefDecAdt")).click();
 		}
 		driver.findElement(By.xpath("//input[@value='Done']")).click();	
-		Assert.assertEquals(dropdown.getText(), "5 Adult");
+		Assert.assertEquals(dropdown.getText(), "1 Adult");
 		//if you want to print the value in console then go with System.out.println();
 		System.out.println(dropdown.getText());
 		driver.close();
