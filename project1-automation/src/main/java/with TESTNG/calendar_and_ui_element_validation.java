f8f6bafe72234a06f656c0c894448ebbc77d6df3
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-09 21:38:00 +0530

    Create calendar_and_ui_element_validation.java

---
+++ project1-automation/src/main/java/with TESTNG/calendar_and_ui_element_validation.java
@@ -0,0 +1,52 @@
+package automationtestingwithudemy;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Day7_calendar_and_ui_validation_testng {
+	WebDriver driver;
+
+	@BeforeClass
+	public void launch() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void calendarAndUIElementValidation() throws InterruptedException {
+
+		driver.get("https://www.spicejet.com/");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector(".css-1cwyjr8.r-homxoj.r-ubezar.r-10paoce.r-13qz1uu")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[text()='CJB']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[text()='MAA']")).click();
+
+		// to select the current date in calendar
+		driver.findElement(By.cssSelector(
+				".css-1dbjc4n.r-1awozwy.r-19m6qjp.r-156aje7.r-y47klf.r-1phboty.r-1d6rzhh.r-1pi2tsx.r-1777fci.r-13qz1uu"))
+				.click();
+
+		// to check if the element is disabled or not
+		WebElement todate = driver.findElement(By.xpath("//div[@data-testid='return-date-dropdown-label-test-id']"));
+		String styleValue = todate.getDomAttribute("style");
+		System.out.println("the value in the selected attribute: " + styleValue);
+		if (styleValue.contains("background-color: rgb(238, 238, 238)")) {
+			System.out.println("the to date calendary is not enabled");
+			Assert.assertFalse(false);
+		} else {
+			System.out.println("the to date calendary is enabled");
+			Assert.assertFalse(true);
+		}
+		driver.close();
+	}
+}
