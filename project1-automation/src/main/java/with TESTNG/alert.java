a9a735b93cce0255b2bd6e319f46a4e7de7381a8
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-10 22:32:44 +0530

    Create alert.java

---
+++ project1-automation/src/main/java/with TESTNG/alert.java
@@ -0,0 +1,37 @@
+package automationtestingwithudemy;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Day8_alert_testng {
+	static WebDriver driver;
+	@BeforeClass
+	public void launch() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public static void main(String[] args) {
+		String name = "krishnaveni";
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+driver.findElement(By.name("enter-name")).sendKeys("name");
+driver.findElement(By.id("alertbtn")).click();
+String alertValue = driver.switchTo().alert().getText();
+if(alertValue.contains(name)) {
+	System.out.println(alertValue);
+}
+driver.switchTo().alert().accept();
+driver.findElement(By.id("confirmbtn")).click();
+driver.switchTo().alert().dismiss();
+driver.quit();
+	}
+
+}
