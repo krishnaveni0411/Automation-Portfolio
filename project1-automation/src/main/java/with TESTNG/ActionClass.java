19f6bf261fa8955bcd6f382a35e24e5e07ed8796
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-14 23:00:37 +0530

    Create ActionClass.java

---
+++ project1-automation/src/main/java/with TESTNG/ActionClass.java
@@ -0,0 +1,36 @@
+package automationtestingwithudemy;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+public class Day11_ActionClass_TestNG {
+	
+	static WebDriver driver;
+	
+	@BeforeClass
+	public  void launch() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+	}
+	@Test
+	
+	public  void ActionClass() {
+	driver.get("https://www.nykaa.com/");
+	driver.findElement(By.name("search-suggestions-nykaa")).click();
+	Actions action = new Actions(driver);
+	//we are sending the value in capital letter in the search box and the we are double clicking the typed text
+	action.moveToElement(driver.findElement(By.name("search-suggestions-nykaa"))).click().keyDown(Keys.SHIFT).sendKeys("face masks").doubleClick().build().perform();
+	//we are mouse hover on the Eye glasses tab
+	action.moveToElement(driver.findElement(By.id("nykaaNetwork"))).build().perform();
+	// to right click on a element 
+	action.moveToElement(driver.findElement(By.id("nykaaNetwork"))).contextClick().build().perform();
+	
+	}
+}
