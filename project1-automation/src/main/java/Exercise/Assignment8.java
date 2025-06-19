79d9b42925604047977acf09aa2a4fd64e32ce82
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-19 17:53:12 +0530

    Update Assignment8.java

--- project1-automation/src/main/java/Exercise/Assignment8.java
+++ project1-automation/src/main/java/Exercise/Assignment8.java
@@ -4,13 +4,16 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Assignment8 {
 
 	public static void main(String[] args) {
+		String expectedValue = "United Arab Emirates";
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -20,13 +23,17 @@ public class Assignment8 {
 		country_Textbox.sendKeys("uni");
 		List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item']/div"));
 		for (WebElement option : options) { // this is an enhanced for loop
-			if (option.getText().equalsIgnoreCase("United Arab Emirates")) {		
+			if (option.getText().equalsIgnoreCase(expectedValue)) {		
 				option.click();
 				break;
 			}	
 		}
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String selectedValue = (String) js.executeScript("return document.getElementById('autocomplete').value;");
+		System.out.print(selectedValue);
+		Assert.assertEquals(selectedValue, expectedValue);
+		System.out.println("The selected value is the one we choosen");  
 		driver.close();
-
 	}
 
 }
