e444414e9c4db12e9c5ab4a99dfb9f6e48d4c319
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-06-23 23:05:23 +0530

    Create ScreenShot.java

---
+++ project1-automation/src/main/java/with TESTNG/ScreenShot.java
@@ -0,0 +1,42 @@
+package automationtestingwithudemy;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
+import org.apache.commons.io.FileUtils;
+
+public class Day19_ScreenShot_TestNg {
+	static WebDriver driver;
+@BeforeClass
+public void launch() {
+	 driver = new ChromeDriver();
+	driver.manage().window().maximize();
+}
+@Test
+	public  void screenShot() throws IOException, InterruptedException {
+		driver.get("https://www.nykaa.com/");
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(By.name("search-suggestions-nykaa"))).click().keyDown(Keys.SHIFT).sendKeys("Lipstick", Keys.ENTER).build().perform();
+		Thread.sleep(2000); 
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,805)");
+		Thread.sleep(2000);
+		File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(source, new File("E:\\Screenshot form script running//screenshot.png"));
+	}
+@AfterClass
+public void terminate() {
+	driver.quit();
+}
+}
