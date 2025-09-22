af32c6732e11c30ff91174486f532441529d1ed0
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-09-22 12:43:30 +0530

    Update ProductCatalogue.java

--- Project -2 Automation/Selenium Code using Frame Work/src/main/java/POMClass/ProductCatalogue.java
+++ Project -2 Automation/Selenium Code using Frame Work/src/main/java/POMClass/ProductCatalogue.java
@@ -1,4 +1,4 @@
-package POMClass;
+package com.mycompany.app;
 
 import java.util.List;
 
@@ -8,60 +8,46 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import BaseClass.BeforeClass;
 
-import BaseClass.BaseClass;
+public class ProductCatogry extends BeforeClass {
+	//WebDriver driver;
 
-public class ProductCatalogue extends BaseClass {
+
+	public ProductCatogry(WebDriver driver) {
+		super(driver);
+		// this.driver = driver;
+		PageFactory.initElements(driver, this);
 	
-
-		WebDriver driver;
-
-		public ProductCatalogue(WebDriver driver) {
-			// initialization
-			super(driver);
-			this.driver = driver;
-			PageFactory.initElements(driver, this);
-
-		}
-
-		@FindBy(css = ".mb-3")
-		List<WebElement> products;
-		
-		@FindBy(css = ".ng-animating")
-		WebElement spinner;
-
-		By productsBy = By.cssSelector(".mb-3");
-		By addToCart = By.cssSelector(".card-body button:last-of-type");
-		By toastMessage = By.cssSelector("#toast-container");
-
-		public List<WebElement> getProductList() {
-			waitForElementToAppear(productsBy);
-			return products;
-		}
-		
-		public WebElement getProductByName(String productName)
-		{
-			WebElement prod =	getProductList().stream().filter(product->
-			product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst().orElse(null);
-			return prod;
-		}
-		
-		
-		public void addProductToCart(String productName) throws InterruptedException
-		{
-			WebElement prod = getProductByName(productName);
-			prod.findElement(addToCart).click();
-			waitForElementToAppear(toastMessage);
-			waitForElementToDisappear(spinner);
-
-
-		}
-		
-		
-		
-		
-		
-
 	}
-
-
+	@FindBy(css= "*.col-lg-4")
+	List<WebElement> products;
+	
+	By product = By.cssSelector("*.col-lg-4");
+	By addToCart = By.cssSelector(".card-body button:last-of-type");
+	By toastMess = By.cssSelector("*.toast-success");
+	
+	
+	public List<WebElement> getProductList() {
+		waitForWebElementToAppear(product);
+		return products;
+	
+	}
+	public WebElement getProductName(String productName) {
+	WebElement prod = products.stream()
+			.filter(product -> product.findElement(By.cssSelector("b")).getText().equals(productName))
+			.findFirst().orElse(null);
+	return prod;
+	}
+	
+	public CartProducts addProductToCart(String productName) throws InterruptedException {
+		WebElement prod = getProductName(productName);
+		prod.findElement(addToCart).click();
+		waitForWebElementToAppear(toastMess);
+		Thread.sleep(2000);
+		CartProducts cartProducts = new CartProducts(driver);
+		return cartProducts;
+	}
+	
+	
+}
