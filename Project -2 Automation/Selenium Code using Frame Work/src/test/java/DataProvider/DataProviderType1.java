44e3ecb8a560c73fc695fb1e60763d122ba4cf1e
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-09-22 22:34:03 +0530

    Update DataProviderType1.java

--- Project -2 Automation/Selenium Code using Frame Work/src/test/java/DataProvider/DataProviderType1.java
+++ Project -2 Automation/Selenium Code using Frame Work/src/test/java/DataProvider/DataProviderType1.java
@@ -1,1 +1,41 @@
+package com.mycompany.app;
 
+import java.io.IOException;
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import BaseTest.BaseTest;
+
+public class CompleteCodeDataProvider extends BaseTest {
+	@Test (dataProvider= "getData")
+	public void submitOrder(String mailId, String password, String productName) throws InterruptedException, IOException {
+		
+		LandingPage landingPage = launchApplication();
+		ProductCatogry productCatorgy =landingPage.LoingApplication(mailId,password); 
+		List<WebElement> products = productCatorgy.getProductList();
+		productCatorgy.getProductName(productName);
+		CartProducts cartProducts = productCatorgy.addProductToCart(productName);
+		System.out.println("Checking for product in the cart...");
+		cartProducts.getCartProducts().forEach(p -> System.out.println("Product in cart: " + p.getText()));
+		Boolean match = cartProducts.cartProducts(productName);
+		Assert.assertTrue(match);
+		cartProducts.checkOut();
+		cartProducts.fillTheForm();
+		String orderconfirmMess =  cartProducts.orderConfirmationPage();
+		System.out.println(orderconfirmMess);
+		Assert.assertTrue(orderconfirmMess.equalsIgnoreCase("Thankyou for the order."));
+		System.out.println("Test completed successfully!");
+		tearDown();
+		
+	}
+	
+	@DataProvider 
+	  public Object[][] getData(){
+	  return new Object[][] {{"krishanveni123@krish.com", "Qazwsx@123", "ADIDAS ORIGINAL"}, {"krish123@krish.com", "Qazwsx@123","ADIDAS ORIGINAL"}};
+	// In this data the second set email and password is wrong so the test will fail. 
+	}
+	
+
+}
