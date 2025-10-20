a6a52a9c9ded7b31bdceca03259a0b6c720bdf5e
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-10-20 19:57:51 +0530

    Update CodeWithGroupingConcept.java

--- Project -2 Automation/Selenium Code using Frame Work/src/test/java/grouping Concept/CodeWithGroupingConcept.java
+++ Project -2 Automation/Selenium Code using Frame Work/src/test/java/grouping Concept/CodeWithGroupingConcept.java
@@ -5,30 +5,40 @@ import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import BaseTest.BaseTest;
 
 public class Groupingthecompletecode extends BaseTest {
 	ProductCatogry productCatorgy;
+	CartProducts cartProducts;
 	String productName = "ADIDAS ORIGINAL";
 	String mailId = "krishanveni123@krish.com";
 	
-	@Test (groups = {"Login"})
+	@BeforeTest (alwaysRun=true)
 	public void login() throws InterruptedException, IOException {
 		LandingPage landingPage = launchApplication();
 		productCatorgy =landingPage.LoingApplication(mailId,"Qazwsx@123");  //in place of mailid replace with generatedMailId if we run register class.
 	}
-	@Test (groups = {"placeAnOrder"})
-	public void submitorder() throws InterruptedException {
+	@Test (groups = {"AddProduct"})
+	public void AddProduct() throws InterruptedException {
 		
 		List<WebElement> products = productCatorgy.getProductList();
 		productCatorgy.getProductName(productName);
-		CartProducts cartProducts = productCatorgy.addProductToCart(productName);
+	}
+		@Test (groups = {"CartPage"})
+		public void CartPage() throws InterruptedException {
+			
+		cartProducts = productCatorgy.addProductToCart(productName);
 		System.out.println("Checking for product in the cart...");
 		cartProducts.getCartProducts().forEach(p -> System.out.println("Product in cart: " + p.getText()));
 		Boolean match = cartProducts.cartProducts(productName);
 		Assert.assertTrue(match);
 		cartProducts.checkOut();
+		}
+		@Test (groups = {"placeAnOrder"})
+		public void submitorder() throws InterruptedException {
+			
 		cartProducts.fillTheForm();
 		String orderconfirmMess =  cartProducts.orderConfirmationPage();
 		System.out.println(orderconfirmMess);
