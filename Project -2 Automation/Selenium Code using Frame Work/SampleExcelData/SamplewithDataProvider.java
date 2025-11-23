1558853b01c5b5d076d614c319aca0b17754edf0
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2025-11-23 19:48:17 +0530

    Add data-driven testing with Excel data provider
    Implement data-driven testing using Excel data provider.

---
+++ Project -2 Automation/Selenium Code using Frame Work/SampleExcelData/SamplewithDataProvider.java
@@ -0,0 +1,48 @@
+package dataDerivenWithDataProvider;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class dataDrivenFromExcelToDataProvider {
+	
+	@Test(dataProvider="dataTest")
+	public void testCase(String gretting, String communication, String ID) {
+		
+	}
+	
+	DataFormatter formatter = new DataFormatter();
+@DataProvider(name = "dataTest")
+public Object[][] getData() throws IOException {
+	
+	FileInputStream file = new FileInputStream("C:\\Users\\Waffor\\Downloads\\Exceldriven.xlsx");
+	XSSFWorkbook workBook = new XSSFWorkbook(file);
+	XSSFSheet sheet = workBook.getSheetAt(0);
+	int rowCount= sheet.getPhysicalNumberOfRows();
+	XSSFRow row =sheet.getRow(0);
+	int columnCount =	row.getLastCellNum();
+	Object data[][] = new Object[rowCount-1][columnCount];
+	for(int i=0; i<rowCount-1;i++) {
+		sheet.getRow(i+1);
+		for(int j=0; j<columnCount; j++) {
+			XSSFCell cell =row.getCell(j);
+			data[i][j] = formatter.formatCellValue(cell);
+			
+		}
+	}
+
+	
+	
+	 //	Object[][] data = {{array1}, {array2}, {array3}};
+	 	return data;
+}
+
+}
+
